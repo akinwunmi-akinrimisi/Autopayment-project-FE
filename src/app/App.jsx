d470eb195855021893567491d5fa8b312e299f5d
Abidoyesimze <similoluwaeyitayoabidoye@gmail.com>
2024-11-13 19:56:23 +0100

    implement the connect btn

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,13 +3,50 @@ import Router from "../router/Router";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  liskSepolia,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+    appName: 'My RainbowKit App',
+    projectId: import.meta.env.VITE_APPKIT_PROJECT_ID,
+    chains: [liskSepolia, polygon, optimism, arbitrum, base],
+    ssr: true, 
+  });
+
+const queryClient = new QueryClient();
+  
+
 
 function App() {
   return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
     <div>
       <Router />
       <ToastContainer />
+      
     </div>
+    </RainbowKitProvider>
+    </QueryClientProvider>
+    </WagmiProvider>
+      
+     
   );
 }
 
