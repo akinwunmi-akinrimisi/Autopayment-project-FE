3ee7f5ae3f2bbe26dfdd8e24341ba4121fc48ebb
ozavcodez <akandeozavize@gmail.com>
2024-11-18 13:11:49 +0100

    add dashboard page

--- src/pages/dashboard/Dashbaord.jsx
+++ src/pages/dashboard/Dashbaord.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { widgetsData } from './components/data'
+import Widget from './components/Widget'
+import Table from './components/Table'
+
 
 const Dashbaord = () => {
   return (
-    <div>
-      Hey Joy, you can now start creating your components!
+    <div className=''>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {widgetsData.map((widget, index) => (
+          <Widget
+            key={index}
+            title={widget.title}
+            amount={widget.amount}
+            colors={widget.colors} // Pass dynamic colors
+            series={widget.series} // Pass dynamic series
+          />
+        ))}
+      </div>
+      <Table/>
     </div>
   )
 }
---
+++ src/pages/dashboard/components/Widget.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import ReactApexChart from 'react-apexcharts'
+
+const Widget = ({
+  title,
+  amount,
+  series,
+  colors
+}) => {
+  const options = {
+    labels: ["success", "Return"],
+    dataLabels: {
+      enabled: false,
+    },
+    colors: colors,
+    legend: {
+      show: false,
+      // itemMargin: {
+      //   horizontal: 18,
+      //   vertical: 0,
+      // },
+      labels: {
+        // colors:  "#CBD5E1"
+        show: false
+      },
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          size: "65%",
+        },
+        stroke: {
+          show: true,
+        width: 0, 
+        },
+      },
+    },
+    stroke: {
+      show: false, // Disable stroke globally
+      width: 0, // Set stroke width to 0
+    },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          legend: {
+            show: false,
+          },
+        },
+      },
+    ],
+  };
+  return (
+    <div className=' rounded-md bg-[#143869] py-3 px-4 w-full max-w-[350px]'>
+      <div className="flex items-center">
+        <div className='flex-1 text-white' >
+          <div className="text-xl font-medium py-2">{title}</div>
+          <div className='text-[28px] font-semibold'>{amount}</div>
+        </div>
+        <div className="flex-1">
+          <div>
+            <ReactApexChart
+              type="donut"
+              height="118"
+              width="100%"
+              options={options}
+              series={series}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Widget
