449df7411887dee633d673e31ee7ca83015d5408
Itodo Simon <71233924+Itodo-S@users.noreply.github.com>
2024-11-20 10:54:34 +0100

    Revert "update"

--- src/pages/adminEscrow/escrow.jsx
+++ src/pages/adminEscrow/escrow.jsx
@@ -7,7 +7,7 @@ import { FlexiscrowContract } from "../../Constant/index";
 
 const AdminDashboard = () => {
   const { address: connectedAddress, isConnected } = useAccount();
-  const ADMIN_ADDRESS = "0x3b7773b6d1603E412f80543ABF4717c993f515DC";
+  const ADMIN_ADDRESS = "0x6BF7d6b94282BD48ff458599aDafA268BcB009FF";
   const [selectedInvoiceId, setSelectedInvoiceId] = useState(null);
   const [escrowDetails, setEscrowDetails] = useState({});
 
