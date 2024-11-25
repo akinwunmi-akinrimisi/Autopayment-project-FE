7a518c9f27e31c6630492a4b11b627bc85f7a626
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-11-25 12:56:05 +0100

    add approval

--- src/Constant/index.jsx
+++ src/Constant/index.jsx
@@ -1,5 +1,6 @@
 import FlexiscrowAbi from "../ABI/Flexiscrow.json";
 import MultisigAbi from "../ABI/Multisig.json";
+import LiskTokenAbi from "../ABI/LiskToken.json";
 import { Abi } from "../ABI/Escrow";
 
 export const FlexiscrowContract = {
@@ -15,4 +16,8 @@ export const FlexiscrowContract = {
   export const MultisigContract = {
     address: "0x2140eF2532a4CB0f1A2399B673F374b7f6289481",
     abi: MultisigAbi,
+  };
+  export const LiskTokenContract = {
+    address: "0xb592fcedC173B15203F03142E4e7584530B45759",
+    abi: LiskTokenAbi,
   };
